cb5b3b225e5efa81a1b26407fe509e247253cb68
SkyeBeFreeman <928016560@qq.com>
2018-01-15 22:26:18 +0800

    update test_screenshoot

--- tests/test_screenshoot.py
+++ tests/test_screenshoot.py
@@ -9,21 +9,19 @@ temp_file = 'screenshot_temp.png'
 
 class TestScreenshot(unittest.TestCase):
 
-    # def test_screenshot(self):
-    #     capture_screen(filename=temp_file)
-    #     img = Image.open(os.path.join('screenshots', temp_file))
-    #     # img.show()
-    #     print('文件格式:', img.format)
-    #     print('文件尺寸:', img.size)
-    #     print('图片模式:', img.mode)
+    def test_screenshot(self):
+        capture_screen(filename=temp_file)
+        img = Image.open(os.path.join('screenshots', temp_file))
+        img.show()
+        print('文件格式:', img.format)
+        print('文件尺寸:', img.size)
+        print('图片模式:', img.mode)
 
 
     def test_imagecrop(self):
         process_image(filename='screenshot_test.png')
         Image.open(os.path.join('screenshots', 'screenshot_question.png')).show()
-        Image.open(os.path.join('screenshots', 'screenshot_a.png')).show()
-        Image.open(os.path.join('screenshots', 'screenshot_b.png')).show()
-        Image.open(os.path.join('screenshots', 'screenshot_c.png')).show()
+        Image.open(os.path.join('screenshots', 'screenshot_choices.png')).show()
 
 
     def tearDown(self):
