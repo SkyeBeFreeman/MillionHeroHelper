96c8a8ba1dc85181e34be47448a0e6bbea5c5b30
SkyeBeFreeman <928016560@qq.com>
2018-01-16 00:15:38 +0800

    添加读取配置文件功能

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from core.screenshot import capture_screen
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import strftime, clock
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config/config.conf')
+app_id = config.get('config', 'app_id')
+api_key = config.get('config', 'api_key')
+secret_key = config.get('config', 'secret_key')
 
 browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
 browser.get('https://www.baidu.com')
@@ -18,21 +25,22 @@ while True:
 
     capture_screen()
 
-    o = orc(app_id='10689024', api_key='ABTXXPeylq1B6cUp0CdDoxlP', secret_key='dnuV0MfxVI9xIZtewfdA3qutGTPAflzS')
+    o = orc(app_id=app_id, api_key=api_key, secret_key=secret_key)
 
     question, choices = o.get_result()
     keyword = question + ' '
     for s in choices:
         keyword = keyword + s + ' '
 
-    print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索", flush=True)
-    print(keyword)
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索：" + keyword, flush=True)
     elem = browser.find_element_by_id("kw")
     elem.clear()
     elem.send_keys(keyword)
     elem.send_keys(Keys.RETURN)
     print(strftime("%Y-%m-%d %H:%M:%S") + " 结束搜索", flush=True)
     cnt = clock() - start
-    print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "s", flush=True)
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "秒", flush=True)
 
-    print('----------------------------------')
+    print('----------------------------------------')
+
+browser.quit()
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,13 @@ from time import strftime, clock
 from context import core
 from core.orc import orc
 from core.screenshot import process_image
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config/config.conf')
+app_id = config.get('config', 'app_id')
+api_key = config.get('config', 'api_key')
+secret_key = config.get('config', 'secret_key')
 
 browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
 browser.get('https://www.baidu.com')
@@ -13,15 +20,15 @@ start = clock()
 
 process_image(filename='screenshot_test.png')
 
-o = orc(app_id='10689024', api_key='ABTXXPeylq1B6cUp0CdDoxlP', secret_key='dnuV0MfxVI9xIZtewfdA3qutGTPAflzS')
+o = orc(app_id=app_id, api_key=api_key, secret_key=secret_key)
+
 
 question, choices = o.get_result()
 keyword = question + ' '
 for s in choices:
     keyword = keyword + s + ' '
 
-print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索", flush=True)
-print(keyword)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索：" + keyword, flush=True)
 elem = browser.find_element_by_id("kw")
 elem.clear()
 elem.send_keys(keyword)
@@ -29,3 +36,5 @@ elem.send_keys(Keys.RETURN)
 print(strftime("%Y-%m-%d %H:%M:%S") + " 结束搜索", flush=True)
 cnt = clock() - start
 print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "s", flush=True)
+
+browser.quit()
