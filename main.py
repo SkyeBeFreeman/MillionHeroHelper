01d166ada290092d20c7b7a0b3a21b4e6c255cac
SkyeBeFreeman <928016560@qq.com>
2018-01-16 13:11:37 +0800

    修改为键盘监听hook方式处理键盘按键事件

--- main.py
+++ main.py
@@ -4,22 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import strftime, clock
 import configparser
+import win32gui
+import sys, os
+from pyhooked import Hook, KeyboardEvent
 
-config = configparser.ConfigParser()
-config.read('config/config.conf')
-app_id = config.get('config', 'app_id')
-api_key = config.get('config', 'api_key')
-secret_key = config.get('config', 'secret_key')
-
-browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
-browser.get('https://www.baidu.com')
-
-while True:
-
-    go = input('输入回车继续运行,输入 n 回车结束运行: ')
-    if go == 'n':
-        break
-
+def runner():
     print(strftime("%Y-%m-%d %H:%M:%S") + " 开始答题", flush=True)
     start = clock()
 
@@ -40,7 +29,34 @@ while True:
     print(strftime("%Y-%m-%d %H:%M:%S") + " 结束搜索", flush=True)
     cnt = clock() - start
     print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "秒", flush=True)
+    print('\n--------------------按回车继续,按Q结束---------------------\n')    
 
-    print('----------------------------------------')
 
-browser.quit()
+def events_handler(args):
+    if isinstance(args, KeyboardEvent):
+        if args.current_key == 'Return' and args.event_type == 'key down':
+            runner()
+        elif args.current_key == 'Q' and args.event_type == 'key down':
+            browser.quit()
+            hook.stop()
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config/config.conf')
+    app_id = config.get('config', 'app_id')
+    api_key = config.get('config', 'api_key')
+    secret_key = config.get('config', 'secret_key')
+
+    browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
+    browser.get('https://www.baidu.com')
+
+    vm = win32gui.FindWindow(None, '夜神模拟器')
+    if vm <= 0:
+        print('未启动夜神模拟器')
+        while vm <= 0:
+            vm = win32gui.FindWindow(None, '夜神模拟器')
+    print('\n--------------------按回车继续,按Q结束---------------------\n')
+    pid = os.getpid()
+    hook = Hook()
+    hook.handler = events_handler
+    hook.hook()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,36 +5,60 @@ from context import core
 from core.orc import orc
 from core.screenshot import process_image
 import configparser
+import win32gui
+import sys, os
+from pyhooked import Hook, KeyboardEvent
 
-config = configparser.ConfigParser()
-config.read('config/config.conf')
-app_id = config.get('config', 'app_id')
-api_key = config.get('config', 'api_key')
-secret_key = config.get('config', 'secret_key')
+def runner():
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 开始答题", flush=True)
+    start = clock()
 
-browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
-browser.get('https://www.baidu.com')
+    process_image()
 
-print(strftime("%Y-%m-%d %H:%M:%S") + " 开始答题", flush=True)
-start = clock()
+    o = orc(app_id=app_id, api_key=api_key, secret_key=secret_key)
 
-process_image(filename='screenshot_test.png')
+    question, choices = o.get_result()
+    keyword = question + ' '
+    for s in choices:
+        keyword = keyword + s + ' '
 
-o = orc(app_id=app_id, api_key=api_key, secret_key=secret_key)
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索：" + keyword, flush=True)
+    elem = browser.find_element_by_id("kw")
+    elem.clear()
+    elem.send_keys(keyword)
+    elem.send_keys(Keys.RETURN)
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 结束搜索", flush=True)
+    cnt = clock() - start
+    print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "秒", flush=True)
+    print('--------------------按C继续运行,按Q结束运行---------------------')    
+    print()
 
 
-question, choices = o.get_result()
-keyword = question + ' '
-for s in choices:
-    keyword = keyword + s + ' '
+def handle_events(args):
+    if isinstance(args, KeyboardEvent):
+        if args.current_key == 'C' and args.event_type == 'key down':
+            runner()
+        elif args.current_key == 'Q' and args.event_type == 'key down':
+            browser.quit()
+            hook.stop()
+            os.exit()
 
-print(strftime("%Y-%m-%d %H:%M:%S") + " 开始搜索：" + keyword, flush=True)
-elem = browser.find_element_by_id("kw")
-elem.clear()
-elem.send_keys(keyword)
-elem.send_keys(Keys.RETURN)
-print(strftime("%Y-%m-%d %H:%M:%S") + " 结束搜索", flush=True)
-cnt = clock() - start
-print(strftime("%Y-%m-%d %H:%M:%S") + " 结束答题，用时" + str(cnt) + "s", flush=True)
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config/config.conf')
+    app_id = config.get('config', 'app_id')
+    api_key = config.get('config', 'api_key')
+    secret_key = config.get('config', 'secret_key')
 
-browser.quit()
+    browser = webdriver.Chrome(r'.\utils\chromedriver.exe')
+    browser.get('https://www.baidu.com')
+
+    vm = win32gui.FindWindow(None, '夜神模拟器')
+    if vm <= 0:
+        print('未启动夜神模拟器')
+        while vm <= 0:
+            vm = win32gui.FindWindow(None, '夜神模拟器')
+    print('--------------------按C继续运行,按Q结束运行---------------------')
+    hook = Hook()
+    hook.handler = handle_events
+    hook.hook()
